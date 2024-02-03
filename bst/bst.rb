3980ba36c484859fbb003c9d606147e1c9b015c4
vdev60 <dved490@gmail.com>
2024-02-03 19:24:13 +0200

    Init Binary Search Tree

---
+++ bst/bst.rb
@@ -0,0 +1,17 @@
+# frozen_string_literal: true
+
+class Node
+  attr_accessor :value, :left, :right
+
+  def initialize(value)
+    @value = value
+    @left = nil
+    @right = nil
+  end
+end
+
+class BinarySearchTree
+  def initialize
+    @root = nil
+  end
+end
